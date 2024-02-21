e1d6340996a421ecbf6db1a6cfc6caf660c5142f
hungdaqq <hungdaqq@gmail.com>
2024-02-21 08:18:56 +0700

    update reparameterization

--- models/MyModel.py
+++ models/MyModel.py
@@ -33,25 +33,173 @@ def conv_2d(inp, oup, kernel_size=3, stride=1, padding=0, groups=1, bias=False, 
     return conv_block
 
 class InvertedResidual(nn.Module):
-    def __init__(self, inp, oup, stride, expand_ratio):
+    def __init__(self, inp, oup, stride, expand_ratio, kernel_size, num_conv_branches, inference_mode):
         super(InvertedResidual, self).__init__()
         self.stride = stride
+        self.kernel_size = kernel_size
+        self.num_conv_branches = num_conv_branches
+        self.inference_mode = inference_mode
         assert stride in [1, 2]
         # hidden_dim = int(round(inp * expand_ratio))
         hidden_dim = int(round(inp * expand_ratio))
+
         self.block = nn.Sequential()
         if expand_ratio != 1:
             self.block.add_module('exp_1x1', conv_2d(inp, hidden_dim, kernel_size=1, stride=1, padding=0))
-        self.block.add_module('conv_3x3', conv_2d(hidden_dim, hidden_dim, kernel_size=3, stride=stride, padding=1, groups=hidden_dim))
-        self.block.add_module('red_1x1', conv_2d(hidden_dim, oup, kernel_size=1, stride=1, padding=0, act=False))
-        self.use_res_connect = self.stride == 1 and inp == oup
-
-    def forward(self, x):
-        if self.use_res_connect:
-            return x + self.block(x)
+        # self.block.add_module('conv_3x3', conv_2d(hidden_dim, hidden_dim, kernel_size=3, stride=stride, padding=1, groups=hidden_dim))
+        # self.block.add_module('red_1x1', conv_2d(hidden_dim, oup, kernel_size=1, stride=1, padding=0, act=False))
+        # self.use_res_connect = self.stride == 1 and inp == oup
+        self.activation = h_swish()
+        if inference_mode:
+            self.reparam_conv = nn.Conv2d(in_channels=inp,
+                                          out_channels=oup,
+                                          stride=stride)
         else:
-            return self.block(x)  
+            # Re-parameterizable skip connection
+            self.rbr_skip = nn.BatchNorm2d(num_features=inp) \
+                if oup == inp and stride == 1 else None
+            # Re-parameterizable conv branches
+            rbr_conv = list()
+            for _ in range(self.num_conv_branches):
+                rbr_conv.append(conv_2d(kernel_size=kernel_size))
+            self.rbr_conv = nn.ModuleList(rbr_conv)
 
+            # Re-parameterizable scale branch
+            self.rbr_scale = None
+            if kernel_size > 1:
+                self.rbr_scale = conv_2d(kernel_size=1, padding=0)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """ Apply forward pass. """
+        # Inference mode forward pass.
+        if self.inference_mode:
+            return self.activation(self.se(self.reparam_conv(x)))
+
+        # Multi-branched train-time forward pass.
+        # Skip branch output
+        identity_out = 0
+        if self.rbr_skip is not None:
+            identity_out = self.rbr_skip(x)
+
+        # Scale branch output
+        scale_out = 0
+        if self.rbr_scale is not None:
+            scale_out = self.rbr_scale(x)
+
+        # Other branches
+        out = scale_out + identity_out
+        for ix in range(self.num_conv_branches):
+            out += self.rbr_conv[ix](x)
+
+        return self.activation(self.se(out))
+    
+    # def forward(self, x):
+    #     if self.use_res_connect:
+    #         return x + self.block(x)
+    #     else:
+    #         return self.block(x)  
+    def reparameterize(self):
+        """ Following works like `RepVGG: Making VGG-style ConvNets Great Again` -
+        https://arxiv.org/pdf/2101.03697.pdf. We re-parameterize multi-branched
+        architecture used at training time to obtain a plain CNN-like structure
+        for inference.
+        """
+        if self.inference_mode:
+            return
+        kernel, bias = self._get_kernel_bias()
+        self.reparam_conv = nn.Conv2d(in_channels=self.rbr_conv[0].conv.in_channels,
+                                      out_channels=self.rbr_conv[0].conv.out_channels,
+                                      kernel_size=self.rbr_conv[0].conv.kernel_size,
+                                      stride=self.rbr_conv[0].conv.stride,
+                                      padding=self.rbr_conv[0].conv.padding,
+                                      dilation=self.rbr_conv[0].conv.dilation,
+                                      groups=self.rbr_conv[0].conv.groups,
+                                      bias=True)
+        self.reparam_conv.weight.data = kernel
+        self.reparam_conv.bias.data = bias
+
+        # Delete un-used branches
+        for para in self.parameters():
+            para.detach_()
+        self.__delattr__('rbr_conv')
+        self.__delattr__('rbr_scale')
+        if hasattr(self, 'rbr_skip'):
+            self.__delattr__('rbr_skip')
+
+        self.inference_mode = True
+
+    def _get_kernel_bias(self):
+        """ Method to obtain re-parameterized kernel and bias.
+        Reference: https://github.com/DingXiaoH/RepVGG/blob/main/repvgg.py#L83
+
+        :return: Tuple of (kernel, bias) after fusing branches.
+        """
+        # get weights and bias of scale branch
+        kernel_scale = 0
+        bias_scale = 0
+        if self.rbr_scale is not None:
+            kernel_scale, bias_scale = self._fuse_bn_tensor(self.rbr_scale)
+            # Pad scale branch kernel to match conv branch kernel size.
+            pad = self.kernel_size // 2
+            kernel_scale = torch.nn.functional.pad(kernel_scale,
+                                                   [pad, pad, pad, pad])
+
+        # get weights and bias of skip branch
+        kernel_identity = 0
+        bias_identity = 0
+        if self.rbr_skip is not None:
+            kernel_identity, bias_identity = self._fuse_bn_tensor(self.rbr_skip)
+
+        # get weights and bias of conv branches
+        kernel_conv = 0
+        bias_conv = 0
+        for ix in range(self.num_conv_branches):
+            _kernel, _bias = self._fuse_bn_tensor(self.rbr_conv[ix])
+            kernel_conv += _kernel
+            bias_conv += _bias
+
+        kernel_final = kernel_conv + kernel_scale + kernel_identity
+        bias_final = bias_conv + bias_scale + bias_identity
+        return kernel_final, bias_final
+
+    def _fuse_bn_tensor(self, branch) -> Tuple[torch.Tensor, torch.Tensor]:
+        """ Method to fuse batchnorm layer with preceeding conv layer.
+        Reference: https://github.com/DingXiaoH/RepVGG/blob/main/repvgg.py#L95
+
+        :param branch:
+        :return: Tuple of (kernel, bias) after fusing batchnorm.
+        """
+        if isinstance(branch, nn.Sequential):
+            kernel = branch.conv.weight
+            running_mean = branch.bn.running_mean
+            running_var = branch.bn.running_var
+            gamma = branch.bn.weight
+            beta = branch.bn.bias
+            eps = branch.bn.eps
+        else:
+            assert isinstance(branch, nn.BatchNorm2d)
+            if not hasattr(self, 'id_tensor'):
+                input_dim = self.in_channels // self.groups
+                kernel_value = torch.zeros((self.in_channels,
+                                            input_dim,
+                                            self.kernel_size,
+                                            self.kernel_size),
+                                           dtype=branch.weight.dtype,
+                                           device=branch.weight.device)
+                for i in range(self.in_channels):
+                    kernel_value[i, i % input_dim,
+                                 self.kernel_size // 2,
+                                 self.kernel_size // 2] = 1
+                self.id_tensor = kernel_value
+            kernel = self.id_tensor
+            running_mean = branch.running_mean
+            running_var = branch.running_var
+            gamma = branch.weight
+            beta = branch.bias
+            eps = branch.eps
+        std = (running_var + eps).sqrt()
+        t = (gamma / std).reshape(-1, 1, 1, 1)
+        return kernel * t, beta - running_mean * gamma / std
 
 class Attention(nn.Module):
     def __init__(self, embed_dim, heads=4, dim_head=8, attn_dropout=0):
